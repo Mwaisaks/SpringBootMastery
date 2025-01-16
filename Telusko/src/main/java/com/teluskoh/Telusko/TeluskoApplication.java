4c46463311e033dfaa8f0d36800b331a67aa741f
Phenny <mwaisakaphenny@gmail.com>
2025-01-16 22:56:07 +0300

    IOC and DI, Autowiring, Application with no dependencies

---
+++ Telusko/src/main/java/com/teluskoh/Telusko/TeluskoApplication.java
@@ -0,0 +1,13 @@
+package com.teluskoh.Telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluskoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeluskoApplication.class, args);
+	}
+
+}
